309741617eaca8df7aab048ab3cf6daf04447304
SANGILI KANNAN V <30505795+sangili-14@users.noreply.github.com>
2018-06-27 10:09:22 +0530

    Create Guvi101.java

---
+++ Guvi101.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Guvi101
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        int n=sc.nextInt();
+        String f=s.substring(n);
+        System.out.println(f);
+    }
+}
