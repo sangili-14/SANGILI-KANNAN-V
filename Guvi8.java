6c621001644a70c84c425f2c6015e1b5073e694b
SANGILI KANNAN V <30505795+sangili-14@users.noreply.github.com>
2018-06-27 09:55:23 +0530

    Create Guvi8.java

---
+++ Guvi8.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Guvi8
+{
+    public static void main(String args[])
+    {
+        int sum=0;
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            sum=sum+i;
+        }
+        System.out.println(sum);
+    }
+}
