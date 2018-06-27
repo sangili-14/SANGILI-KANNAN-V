8fbb2272d7b0bbaa8eae83ae150d9c86990d7de5
SANGILI KANNAN V <30505795+sangili-14@users.noreply.github.com>
2018-06-27 09:48:32 +0530

    Create Guvi6.java

---
+++ Guvi6.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Guvi6
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%4==0)
+        {
+            System.out.println("yes");
+        }
+        else
+        System.out.println("no");
+    }
+}
