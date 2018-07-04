9a5aa2e1b0779697ccb656b295407d9716a5645f
SANGILI KANNAN V <30505795+sangili-14@users.noreply.github.com>
2018-07-04 23:35:44 +0530

    Create h124.java

---
+++ h124.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class h124
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=n;i>0;i--)
+        {
+            for(int j=0;j<i;j++)
+            {
+                System.out.print("1");
+            }
+            System.out.println("");
+        }
+    }    
+}
